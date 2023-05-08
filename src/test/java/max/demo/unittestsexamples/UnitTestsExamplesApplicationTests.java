2b6d4cd299edd8712a7aef6547dd00718a195ecf
max <maxim_ovidiu_alex@yahoo.com>
2023-05-08 22:08:04 +0300

    - add working spring boot project

---
+++ src/test/java/max/demo/unittestsexamples/UnitTestsExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package max.demo.unittestsexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestsExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
